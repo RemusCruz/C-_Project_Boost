a98c6731578509a20209bd424d138832ee9c7597
LAPTOP-JEHD2JGD\Kieran <remus.cruz@gmail.com>
2018-10-04 10:20:29 +0100

    PB.04

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space Pressed");
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            print("Right Arrow Pressed");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("Left Arrow Key Pressed");
+        }
+    }
+}
